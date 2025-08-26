88f644de8012dc557afd217d4d7222b624d74624
LaithHammad <126326799+LaithHammad@users.noreply.github.com>
2025-08-26 21:46:01 +0300

    Delete CTF4 directory

--- CTF4/tes.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("redx.db")
-cursor = conn.cursor()
-cursor.execute("SELECT username, password FROM employees WHERE username='admin'")
-row = cursor.fetchone()
-print("Stored admin password hash:", row[1])
-
